129e17a5dfa0ac7524d8d475b3b82acf22c07428
Coty Beasley <coty@cotybeasley.name>
2015-07-20 13:21:59 -0500

    Angular-ized the youtube video to work in a controller.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -40,6 +40,54 @@
 
 
 
+// Youtube Directive
+app.directive('youtube', function($window) {
+    return {
+        restrict: "E",
+
+        scope: {
+            height:   "@",
+            width:    "@",
+            videoId:  "@"
+        },
+
+        template: '<div></div>',
+
+        link: function(scope, element) {
+            var tag = document.createElement('script');
+            tag.src = "https://www.youtube.com/iframe_api";
+            var firstScriptTag = document.getElementsByTagName('script')[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+            var player;
+
+            $window.onYouTubeIframeAPIReady = function() {
+                player = new YT.Player(element.children()[0], {
+
+                    playerVars: {
+                        autoplay: 0,
+                        html5: 1,
+                        theme: "dark",
+                        modesbranding: 0,
+                        color: "white",
+                        iv_load_policy: 3,
+                        rel: 0,
+                        showinfo: 0,
+                        controls: 2
+                    },
+
+                    height: "1280",
+                    width: "720",
+                    videoId: "vmiUGJtcUi4"
+                });
+            };
+        }
+    };
+});
+
+
+
+
 // Controllers
     app.controller('HomeCtrl', function HomeCtrl($scope) {
         $scope.crowdfundingStatus = true;
